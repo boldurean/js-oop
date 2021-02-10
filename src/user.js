74fc8504d5f7d567f797ca06a9d86d568d8f48a7
vsbdn <v.boldurean@gmail.com>
2021-02-10 12:34:55 +0200

    add: users.js/tests

---
+++ src/user.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/prefer-default-export
+export const getMutualFriends = (user1, user2) => {
+  const friends1 = user1.getFriends();
+  const friends2 = user2.getFriends();
+  return friends1.filter(
+    (friend1) => friends2.some((friend2) => friend2.id === friend1.id),
+  );
+};
